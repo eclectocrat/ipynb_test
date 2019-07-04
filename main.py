04d2707266225099665188580310cfd203b6cf1f
Jeremy Jurksztowicz <jurksztowicz@gmail.com>
2019-07-04 14:43:37 -0400

    files

---
+++ main.py
@@ -0,0 +1,24 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.4'
+#       jupytext_version: 1.1.7
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# +
+# Hello!
+# -
+
+def fun():
+    print("fun()")
+fun()
+
+
