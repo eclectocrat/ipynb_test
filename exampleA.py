ab0dba030bae45da189c3bd9d072f71a9f445a7b
Jeremy Jurksztowicz <jurksztowicz@gmail.com>
2019-07-04 14:16:25 -0400

    w

---
+++ exampleA.py
@@ -0,0 +1,34 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.4'
+#       jupytext_version: 1.1.7
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# Here is a function
+def some_stuff():
+    print("changed lots of stuff stuff!")
+# Hi!
+some_stuff()
+
+# Added too!
+num = 3
+another = "str"
+for _ in range(num):
+    some_stuff()
+
+print("Hello, there!")
+
+# +
+# Comment cell
+# -
+
+
